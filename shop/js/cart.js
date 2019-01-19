bc6bb072d0ae96d32e62c40c7a489522f0403c74
HedgeDA <faraon.da@gmail.com>
2019-01-20 00:50:23 +0600

    ...

--- shop/js/cart.js
+++ shop/js/cart.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function addButtonClick(event) {
+  if (!event.target.classList.contains('add-to-cart')) {
+    return false;
+  }
+
+  addToCart({
+    'title': event.target.dataset.title,
+    'price': event.target.dataset.price
+  })
+}
+
+function init() {
+  document.querySelector('.items-list').addEventListener('click', addButtonClick);
+}
+
+document.addEventListener('DOMContentLoaded', init);
